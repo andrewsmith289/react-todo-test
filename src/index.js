39a3be7675dfc0984d32429451734aa37710e6c0
andrewsmith289 <andrew.c.smith289@gmail.com>
2019-12-13 15:50:39 -0500

    Basic todo list complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,93 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      term: '',
+    };
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render() {
+    return (
+      <div className="todo-list">
+        <TaskAdder
+          onChange={this.onChange}
+          addTask={this.addTask}
+          term={this.state.term}
+        />
+
+        <TaskList tasks={this.state.tasks} />
+      </div>
+    );
+  }
+
+  onChange = event => {
+    this.setState({
+      term: event.target.value,
+    });
+  };
+
+  addTask = () => {
+    console.log('yolo');
+    const newTask = this.state.term;
+
+    if (newTask.length === 0) return;
+
+    this.setState(state => {
+      return {
+        tasks: state.tasks.concat(newTask),
+        term: '',
+      };
+    });
+  };
+}
+
+class TaskAdder extends React.Component {
+  render() {
+    return (
+      <div className="task-adder">
+        <input
+          onChange={e => this.props.onChange(e)}
+          value={this.props.term}
+          type="text"
+          name="taskName"
+          id="task_name"
+        />
+        <button onClick={this.props.addTask}>Add Task</button>
+      </div>
+    );
+  }
+}
+
+class TaskList extends React.Component {
+  render() {
+    const tasks = this.props.tasks;
+    const listItems = tasks.map((task, index) => <li key="{index}">{task}</li>);
+    return (
+      <div className="task-list">
+        <h2>Task List</h2>
+        <ul>{listItems}</ul>
+      </div>
+    );
+  }
+}
+
+class TaskStatusIndicator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false,
+    };
+  }
+  render() {
+    return <div className="task-status-indicator"></div>;
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
